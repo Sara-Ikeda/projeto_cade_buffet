15bf24e1bfcc4e1a50aa54b6d73a3d688f59264a
Sara Ikeda <sara.ikeda10@gmail.com>
2024-05-08 16:00:32 -0300

    Acrescento opção para filtrar API de listagem de buffets pelo nome

--- app/controllers/api/v1/buffets_controller.rb
+++ app/controllers/api/v1/buffets_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::BuffetsController < Api::V1::ApiController
   def index
-    buffets = Buffet.all
+    search = params.permit(:search)
+    if search.present?
+      buffets = Buffet.where("trade_name LIKE '%#{search[:search]}%'")
+    else
+      buffets = Buffet.all
+    end
     if buffets.any?
-      render status: 200, json: buffets, only: [:id, :trade_name]
+      render status: 200, json: buffets, only: [:id, :trade_name], methods: [:contact]
     else
       render status: 204
     end
--- spec/requests/apis/warehouses_api_spec.rb
+++ spec/requests/apis/warehouses_api_spec.rb
@@ -25,7 +25,9 @@ describe 'Buffet API' do
       expect(response.content_type).to include 'application/json'
       json_response = JSON.parse(response.body)
       expect(json_response.first["trade_name"]).to  eq 'Gourmet dos Noivos'
+      expect(json_response.first["contact"]).to  eq '55961524798 | noivos@contato.com'
       expect(json_response.last["trade_name"]).to  eq 'Doces & Salgados SP'
+      expect(json_response.last["contact"]).to  eq '55985943684 | sac@docesesalgados.com'
     end
 
     it 'sucesso porém não há buffets cadastrados' do
@@ -40,6 +42,40 @@ describe 'Buffet API' do
       get '/api/v1/buffets'
 
       expect(response.status).to eq 500
+    end
+  end
+
+  context 'GET /api/v1/buffets?search=value' do
+    it 'sucesso: filtra  a lista de buffets pelo nome' do
+      owner = Owner.create!(email: 'sara@email.com', password: 'password')
+      address = Address.create!(street: 'Av Paulista', number: 50, district: 'Bela Vista',
+                  city: 'São Paulo', state: 'SP', zip: '01153000')
+      buffet = Buffet.create!(trade_name: 'Gourmet dos Noivos', company_name: 'Buffet Gourmet LTDA',
+                registration_number: '56862478000652', telephone: '55961524798',
+                email: 'noivos@contato.com', address: address, owner: owner,
+                description: 'Buffet especializado em casamento', payment_types: 'Cartão Débito/Crédito')
+
+      other_owner = Owner.create!(email: 'ikeda@gmail.com', password: '112358')
+      other_address = Address.create!(street: 'Avenida 9 de Julho', number: 150, district: 'Jardim Paulista',
+                        city: 'São Paulo', state: 'SP', zip: '01153000')
+      other_buffet = Buffet.create!(trade_name: 'Doces & Salgados SP', company_name: 'Doces e Salgados LA',
+                      registration_number: '02946813000245', telephone: '55985943684',
+                      email: 'sac@docesesalgados.com', address: other_address, owner: other_owner,
+                      description: 'Doces e Salgados para sua festa', payment_types: 'Dinheiro e PIX')
+
+      get '/api/v1/buffets?search=Doces'
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response.first["trade_name"]).to  eq 'Doces & Salgados SP'
+      expect(json_response.first["trade_name"]).to_not  eq 'Gourmet dos Noivos'
+    end
+
+    it 'sucesso porém não há buffets com o nome buscado' do
+      get '/api/v1/buffets?search=Doces'
+
+      expect(response.status).to eq 204
     end
   end
 end
