103b0bf5b6e29ad32ba72ddcdae2154b88ec0972
Sara Ikeda <sara.ikeda10@gmail.com>
2024-05-10 14:56:30 -0300

    Inserido gem rack-cors para consumir API na versão VueJS da apicação

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-owner = Owner.create!(email: 'sara@email.com', password: 'password')
+owner = Owner.create!(email: 'buffet1@email.com', password: 'password')
 address = Address.create!(street: 'Av Paulista', number: 50, district: 'Bela Vista',
             city: 'São Paulo', state: 'SP', zip: '01153000')
 buffet = Buffet.create!(trade_name: 'Gourmet dos Noivos', company_name: 'Buffet Gourmet LTDA',
@@ -18,22 +18,36 @@ event_a = Event.create!(name: 'Festa de Casamento', event_description:
           'Todos os serviços para o seu casamento perfeito.', minimum_of_people: 100, maximum_of_people: 250,
           duration: 180, menu: 'Bolo, bem-casadinhos, salgados. Estrogonofe, Carne ao molho madeira.',
           alcoholic_drink: 1, ornamentation: 1, valet: 1, locality: 0, buffet: buffet)
-price = Price.create!(minimum_cost: 10000, add_cost_by_person: 200,
-          add_cost_by_hour: 300 ,weekday: 'Dias Úteis', event: event_a)
+Price.create!(minimum_cost: 5000, add_cost_by_person: 100,
+          add_cost_by_hour: 500 ,weekday: 'Dias Úteis', event: event_a)
+Price.create!(minimum_cost: 8000, add_cost_by_person: 150,
+          add_cost_by_hour: 500 ,weekday: 'Fim de Semana', event: event_a)
 event_b = Event.create!(name: 'Festa de Bodas', event_description: 
           'A festa do seu jeito.', minimum_of_people: 50, maximum_of_people: 150,
           duration: 120, menu: 'Bolo. Jantar.',
           alcoholic_drink: 1, ornamentation: 1, valet: 0, locality: 0, buffet: buffet)
+Price.create!(minimum_cost: 2000, add_cost_by_person: 80,
+          add_cost_by_hour: 200 ,weekday: 'Dias Úteis', event: event_b)
+Price.create!(minimum_cost: 4000, add_cost_by_person: 100,
+          add_cost_by_hour: 300 ,weekday: 'Fim de Semana', event: event_b)
 
-other_owner = Owner.create!(email: 'ikeda@gmail.com', password: '112358')
-other_address = Address.create!(street: 'Avenida 9 de Julho', number: 150, district: 'Jardim Paulista',
+second_owner = Owner.create!(email: 'buffet2@gmail.com', password: 'password')
+second_address = Address.create!(street: 'Avenida 9 de Julho', number: 150, district: 'Jardim Paulista',
                   city: 'São Paulo', state: 'SP', zip: '01153000')
-other_buffet = Buffet.create!(trade_name: 'Doces & Salgados SP', company_name: 'Doces e Salgados LA',
+second_buffet = Buffet.create!(trade_name: 'Doces e Salgados SP', company_name: 'Doces e Salgados LA',
                 registration_number: '02946813000245', telephone: '55985943684',
-                email: 'sac@docesesalgados.com', address: other_address, owner: other_owner,
+                email: 'sac@docesesalgados.com', address: second_address, owner: second_owner,
                 description: 'Doces e Salgados para sua festa', payment_types: 'Dinheiro e PIX')
+second_event_a = Event.create!(name: 'Festa de Aniversário', event_description: 
+                'Para todas as idades.', minimum_of_people: 200, maximum_of_people: 250,
+                duration: 240, menu: 'Bolo, brigadeiros, beijinhos. Esfiras de carne, frango, queijo, etc. Coxinhas.',
+                alcoholic_drink: 0, ornamentation: 1, valet: 1, locality: 0, buffet: second_buffet)
+Price.create!(minimum_cost: 3000, add_cost_by_person: 50,
+                add_cost_by_hour: 100 ,weekday: 'Dias Úteis', event: second_event_a)
+Price.create!(minimum_cost: 3500, add_cost_by_person: 70,
+                add_cost_by_hour: 90 ,weekday: 'Fim de Semana', event: second_event_a)
 
-third_owner = Owner.create!(email: 'hime@email.com', password: '135711')
+third_owner = Owner.create!(email: 'buffet3@email.com', password: 'password')
 third_address = Address.create!(street: 'Avenida Central', number: 99, district: 'Itaipu',
                           city: 'Niterói', state: 'RJ', zip: '85875000')
 Buffet.create!(trade_name: 'Doces do RJ', company_name: 'Doces LTDA',
